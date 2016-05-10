6985cec358afc4dbd23fac8bef5a66df613cb5d0
jwinsor566 <jwinsor@pstc.brown.edu>
2016-05-10 14:48:02 -0400

    Updloaded Files

---
+++ WinsorLabs/wwwroot/logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-04-29 18:23:23 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-04-29T18:23:23+00:00	INFO 128.148.47.170	update	Update started by user Super User (994). Old version is 3.5.0.
+2016-04-29T18:23:23+00:00	INFO 128.148.47.170	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.5.1/Joomla_3.5.x_to_3.5.1-Stable-Patch_Package.zip.
+2016-04-29T18:23:24+00:00	INFO 128.148.47.170	update	File Joomla_3.5.x_to_3.5.1-Stable-Patch_Package.zip successfully downloaded.
+2016-04-29T18:23:24+00:00	INFO 128.148.47.170	update	Starting installation of new version.
+2016-04-29T18:23:36+00:00	INFO 128.148.47.170	update	Finalising installation.
+2016-04-29T18:23:37+00:00	INFO 128.148.47.170	update	Ran query from file 3.5.1-2016-03-25. Query text: ALTER TABLE `#__user_keys` MODIFY `user_id` varchar(150) NOT NULL;.
+2016-04-29T18:23:37+00:00	INFO 128.148.47.170	update	Ran query from file 3.5.1-2016-03-29. Query text: UPDATE `#__utf8_conversion` SET `converted` = 0  WHERE (SELECT COUNT(*) FROM `#_.
+2016-04-29T18:23:37+00:00	INFO 128.148.47.170	update	Deleting removed files and folders.
